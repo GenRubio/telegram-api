35ebb2952c4982f4dcfbb05f17a1fc2ac112f360
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2022-12-08 21:43:22 +0100

    WIP :: Update

--- app/Http/Webhooks/MyWebhookHandler.php
+++ app/Http/Webhooks/MyWebhookHandler.php
@@ -2,20 +2,10 @@
 
 namespace App\Http\Webhooks;
 
-use DefStudio\Telegraph\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 
 class MyWebhookHandler extends WebhookHandler
 {
-    //https://defstudio.github.io/telegraph/quickstart/setting-webhook
-    //https://dashboard.ngrok.com/get-started/your-authtoken
-    //https://github.com/defstudio/telegraph/discussions/119#discussioncomment-3162578
-    //ngrok http http://127.0.0.1:8000
-    //protected function handleChatMessage(Stringable $text): void
-    //{
-    //    $this->chat->html("Received: $text")->send();
-    //}
-
     public function patata(): void
     {
         $this->chat->html("Hola mundo")->send();
