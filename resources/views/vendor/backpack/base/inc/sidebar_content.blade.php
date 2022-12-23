9b3203c59d3b5abbda3bf2f9742d59cf65d5d012
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2022-12-23 09:11:56 +0100

    WIP :: Update Dashboard

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,8 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-title">LANG</li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('language') }}"><i class="nav-icon la la-question"></i> Idiomas</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('translation') }}"><i class="nav-icon la la-question"></i> Traducciones</a></li>
 <li class="nav-title">TIENDA</li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('product-model') }}"><i class="nav-icon la la-question"></i> Productos</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('brand') }}"><i class="nav-icon la la-question"></i> Marcas</a></li>
@@ -7,6 +10,4 @@
 <li class="nav-title">TELEGRAM BOT</li>
 <li class="nav-title">CONFIGURACION</li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('api-client') }}"><i class="nav-icon la la-question"></i> API Clientes</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('language') }}"><i class="nav-icon la la-question"></i> Idiomas</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('translation') }}"><i class="nav-icon la la-question"></i> Traducciones</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('customer') }}"><i class="nav-icon la la-question"></i> Customers</a></li>
