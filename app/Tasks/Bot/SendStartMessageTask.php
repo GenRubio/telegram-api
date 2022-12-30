1a7f2415f8327b40e423ac2666fb006cbf0f8101
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2022-12-30 08:59:54 +0100

    test

--- app/Tasks/Bot/SendStartMessageTask.php
+++ app/Tasks/Bot/SendStartMessageTask.php
@@ -21,7 +21,7 @@ class SendStartMessageTask
         $this->telegramBotMessageService = new TelegramBotMessageService();
         $this->key = '1672042240.2779';
         $this->telegramBotMessage = $this->setTelegramBotMessage();
-        $this->clientApiUrl = (new GetApiClientTask($this->chat->chat_id))->run();
+        $this->clientApiUrl = "https://laravel.gma.cat/"; //(new GetApiClientTask($this->chat->chat_id))->run();
     }
 
     public function run()
