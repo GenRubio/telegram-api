0ef4315fff3b72775bac3d23365700e4574ea71d
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-01-13 20:02:20 +0100

    test

--- app/Http/Webhooks/MyWebhookHandler.php
+++ app/Http/Webhooks/MyWebhookHandler.php
@@ -40,7 +40,7 @@ class MyWebhookHandler extends WebhookHandler
         if (!is_null($language)){
             //$this->chat->deleteMessage($this->message->id)->send();
             $this->chat
-            ->html("hola")
+            ->html($this->message->id)
             ->protected()
             ->send();
         }
