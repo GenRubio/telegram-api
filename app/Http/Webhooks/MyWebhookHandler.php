574d8375faa20dd561542faeff02dec5a2941db0
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-01-13 19:26:35 +0100

    test

--- app/Http/Webhooks/MyWebhookHandler.php
+++ app/Http/Webhooks/MyWebhookHandler.php
@@ -7,6 +7,7 @@ use App\Models\BotChat;
 use App\Services\BotChatService;
 use App\Tasks\Bot\SendStartMessageTask;
 use DefStudio\Telegraph\Keyboard\Button;
+use App\Tasks\Bot\SendLanguageMessageTask;
 use DefStudio\Telegraph\Keyboard\Keyboard;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use App\Http\Webhooks\Bot\v2\Bot\ActionSetLanguage;
@@ -14,15 +15,14 @@ use App\Http\Webhooks\Bot\v2\Bot\ActionSetLanguage;
 class MyWebhookHandler extends WebhookHandler
 {
     use ActionSetLanguage;
-    
+
     public function start($reference = null)
     {
         $botChat = (new BotChatService())->getByChatId($this->chat->chat_id);
         if ($botChat->language) {
             (new SendStartMessageTask($this->chat))->run();
-        }
-        else{
-
+        } else {
+            (new SendLanguageMessageTask($this->chat))->run();
         }
         //https://t.me/HQDTiendaProdEsBot?start=3245435
         try {
