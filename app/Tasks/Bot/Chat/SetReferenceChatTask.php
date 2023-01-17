e9ee4fa3befa7201cd09386e24141984c95e80bf
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-01-17 07:16:01 +0100

    test

--- app/Tasks/Bot/Chat/SetReferenceChatTask.php
+++ app/Tasks/Bot/Chat/SetReferenceChatTask.php
@@ -23,12 +23,12 @@ class SetReferenceChatTask
     public function run()
     {
         if (!is_null($this->reference) && empty($this->botChat->reference)) {
-            $affiliate = $this->affiliateService->getByReference($this->reference);
-            if (!is_null($affiliate)) {
-                $this->botChatService->update($this->botChat->chat_id, [
-                    'reference' => $this->reference
-                ]);
-            }
+            //$affiliate = $this->affiliateService->getByReference($this->reference);
+            //if (!is_null($affiliate)) {
+            //    $this->botChatService->update($this->botChat->chat_id, [
+            //        'reference' => $this->reference
+            //    ]);
+            //}
         }
     }
 }
