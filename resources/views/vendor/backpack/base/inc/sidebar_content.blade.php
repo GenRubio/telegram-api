69b5d393ef0bbf9ea3e4e1afd05e91526c7beb46
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-01-26 08:38:39 +0100

    WIP :: New Icons

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -26,13 +26,13 @@
    @if(backpack_user()->officePermission('BotCrudController', 'show')
    || backpack_user()->officePermission('TelegramBotGroupCrudController', 'show'))
    <li class="nav-item nav-dropdown">
-       <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-question"></i> Bots</a>
+       <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon las la-robot"></i> Bots</a>
        <ul class="nav-dropdown-items">
            @if(backpack_user()->officePermission('BotCrudController', 'show'))
-           <li class="nav-item"><a class="nav-link" href="{{ backpack_url('bot') }}"><i class="nav-icon la la-question"></i> Bots</a></li>
+           <li class="nav-item"><a class="nav-link" href="{{ backpack_url('bot') }}"><i class="nav-icon las la-robot"></i> Bots</a></li>
            @endif
            @if(backpack_user()->officePermission('TelegramBotGroupCrudController', 'show'))
-           <li class="nav-item"><a class="nav-link" href="{{ backpack_url('telegram-bot-group') }}"><i class="nav-icon la la-question"></i> Grupos</a></li>
+           <li class="nav-item"><a class="nav-link" href="{{ backpack_url('telegram-bot-group') }}"><i class="nav-icon las la-list"></i> Grupos</a></li>
            @endif
        </ul>
    </li>
