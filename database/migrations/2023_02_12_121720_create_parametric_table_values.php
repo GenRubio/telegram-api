51dd5e597f8e3880be8dfff29a007bf61afe926d
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-02-12 13:23:45 +0100

    WIP :: Parametric Tables

---
+++ database/migrations/2023_02_12_121720_create_parametric_table_values.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parametric_table_values', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parametric_table_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('parameter');
+            $table->boolean('filter')->default(true);
+            $table->boolean('visible')->default(true);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+
+            $table->foreign('parametric_table_id')
+                ->references('id')
+                ->on('parametric_tables')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parametric_table_values');
+    }
+};
