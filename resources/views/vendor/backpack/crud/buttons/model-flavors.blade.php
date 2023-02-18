88e6bd7e5e9dbeda3aa878d615dfd8a32752606d
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-02-18 10:32:45 +0100

    WIP :: Update Icons

--- resources/views/vendor/backpack/crud/buttons/model-flavors.blade.php
+++ resources/views/vendor/backpack/crud/buttons/model-flavors.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ url($crud->route.'/'.$entry->getKey()) }}/product-models-flavor" class="btn btn-sm btn-link" style="color: #7c69ef !important; cursor: pointer !important;">
-    <i class="la la-eye"></i> Sabores
+    <i class="las la-plus"></i> Sabores
 </a>
