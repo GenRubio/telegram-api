d2b71a1320cfd88d7dfab9cbc1ef46675c14df62
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-02-23 15:56:04 +0100

    test

--- app/Http/Controllers/Admin/ParametricTableCrudController.php
+++ app/Http/Controllers/Admin/ParametricTableCrudController.php
@@ -14,7 +14,7 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 class ParametricTableCrudController extends CrudController
 {
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation {
         store as traitStore;
     }
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
@@ -77,28 +77,50 @@ class ParametricTableCrudController extends CrudController
                 'default' => true,
                 'tab' => 'Tabla'
             ],
-            [
-                'name' => 'create_model_table_values',
-                'label' => 'Crear objeto',
-                'type' => 'checkbox',
-                'default' => true,
-                'tab' => 'Configuración (Valores)'
-            ],
-            [
-                'name' => 'create_backpack_table_values',
-                'label' => 'Crear Backpack (CRUD)',
-                'type' => 'checkbox',
-                'default' => false,
-                'tab' => 'Configuración (Valores)'
-            ],
-            [
-                'name' => 'create_hexagonal_table_values',
-                'label' => 'Crear estructura hexagonal',
-                'type' => 'checkbox',
-                'default' => true,
-                'tab' => 'Configuración (Valores)'
-            ],
         ]);
+        if (config('app.locale') === 'local') {
+            $this->crud->addFields([
+                [
+                    'name' => 'create_model_table_values',
+                    'label' => 'Crear objeto',
+                    'type' => 'checkbox',
+                    'default' => true,
+                    'tab' => 'Configuración (Valores)'
+                ],
+                [
+                    'name' => 'create_backpack_table_values',
+                    'label' => 'Crear Backpack (CRUD)',
+                    'type' => 'checkbox',
+                    'default' => false,
+                    'tab' => 'Configuración (Valores)'
+                ],
+                [
+                    'name' => 'create_hexagonal_table_values',
+                    'label' => 'Crear estructura hexagonal',
+                    'type' => 'checkbox',
+                    'default' => true,
+                    'tab' => 'Configuración (Valores)'
+                ],
+            ]);
+        } else {
+            $this->crud->addFields([
+                [
+                    'name' => 'create_model_table_values',
+                    'type' => 'hidden',
+                    'value' => false,
+                ],
+                [
+                    'name' => 'create_backpack_table_values',
+                    'type' => 'hidden',
+                    'value' => false,
+                ],
+                [
+                    'name' => 'create_hexagonal_table_values',
+                    'type' => 'hidden',
+                    'value' => false,
+                ],
+            ]);
+        }
     }
 
     protected function setupUpdateOperation()
@@ -132,11 +154,10 @@ class ParametricTableCrudController extends CrudController
         $createModel = request()->input('create_model_table_values');
         $createBackpackCrud  = request()->input('create_backpack_table_values');
         $createHexagonalStructure  = request()->input('create_hexagonal_table_values');
-        try{
+        try {
             (new CreateParamTableValueTask($tableName, $createModel, $createBackpackCrud, $createHexagonalStructure))->run();
             return $this->traitStore();
-        }
-        catch(GenericException | Exception $e){
+        } catch (GenericException | Exception $e) {
             dd($e);
             Alert::add('error', $e->getMessage())->flash();
             return back();
