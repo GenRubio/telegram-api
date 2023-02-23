7f96eed96a777c0634c57380ccc2a104db0d261c
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-02-23 13:59:21 +0100

    test

--- resources/views/vendor/backpack/crud/columns/btnToggleV2.blade.php
+++ resources/views/vendor/backpack/crud/columns/btnToggleV2.blade.php
@@ -1,18 +1,19 @@
 @php
+    use Illuminate\Support\Str;
     $checked = $entry->{$column['name']} ? true : false;
-    $key = encript($entry->name);
+    $name = $column['name']
 @endphp
 <td>
-    <div id="personal-switch-{{ $entry->id }}_{{ $key }}" class="custom-control custom-switch" data-model="{{ get_class($entry) }}"
+    <div id="personal-switch-{{ $entry->id }}_{{ $name }}" class="custom-control custom-switch" data-model="{{ get_class($entry) }}"
         data-target="{{ $entry->id }}" data-field="{{ $column['name'] }}">
-        <input type="checkbox" class="custom-control-input" id="customSwitch-{{ $entry->id }}-{{ $key }}"
+        <input type="checkbox" class="custom-control-input" id="customSwitch-{{ $entry->id }}-{{ $name }}"
             {{ $checked ? 'checked' : '' }}>
-        <label class="custom-control-label" for="customSwitch-{{ $entry->id }}-{{ $key }}"></label>
+        <label class="custom-control-label" for="customSwitch-{{ $entry->id }}-{{ $name }}"></label>
     </div>
 </td>
 
 <script type="text/javascript">
-    $("#personal-switch-{{ $entry->id }}_{{ $key }}").on('click', function(ev) {
+    $("#personal-switch-{{ $entry->id }}_{{ $name }}").on('click', function(ev) {
         var model = $(this).data('model');
         var id = $(this).data('target');
         var field = $(this).data('field');
@@ -27,10 +28,10 @@
 
             success: function(result) {
                 if (!result.checked){
-                    $('#customSwitch-{{ $entry->id }}-{{ $key }}').prop('checked', false);
+                    $('#customSwitch-{{ $entry->id }}-{{ $name }}').prop('checked', false);
                 }
                 else{
-                    $('#customSwitch-{{ $entry->id }}-{{ $key }}').prop('checked', true);
+                    $('#customSwitch-{{ $entry->id }}-{{ $name }}').prop('checked', true);
                 }
                 new Noty({
                     type: "success",
