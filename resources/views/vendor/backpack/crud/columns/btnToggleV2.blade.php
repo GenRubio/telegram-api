934103cddbcbc8958cf6c06d8e10d76939c28810
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-02-23 13:50:22 +0100

    test

--- resources/views/vendor/backpack/crud/columns/btnToggleV2.blade.php
+++ resources/views/vendor/backpack/crud/columns/btnToggleV2.blade.php
@@ -2,16 +2,16 @@
     $checked = $entry->{$column['name']} ? true : false;
 @endphp
 <td>
-    <div id="personal-switch-{{ $entry->id }}" class="custom-control custom-switch" data-model="{{ get_class($entry) }}"
+    <div id="personal-switch-{{ $entry->id }}_{{ $entry->name }}" class="custom-control custom-switch" data-model="{{ get_class($entry) }}"
         data-target="{{ $entry->id }}" data-field="{{ $column['name'] }}">
         <input type="checkbox" class="custom-control-input" id="customSwitch-{{ $entry->id }}"
             {{ $checked ? 'checked' : '' }}>
-        <label class="custom-control-label" for="customSwitch-{{ $entry->id }}"></label>
+        <label class="custom-control-label" for="customSwitch-{{ $entry->id }}-{{ $entry->name }}"></label>
     </div>
 </td>
 
 <script type="text/javascript">
-    $("#personal-switch-{{ $entry->id }}").on('click', function(ev) {
+    $("#personal-switch-{{ $entry->id }}-{{ $entry->name }}").on('click', function(ev) {
         var model = $(this).data('model');
         var id = $(this).data('target');
         var field = $(this).data('field');
@@ -26,10 +26,10 @@
 
             success: function(result) {
                 if (!result.checked){
-                    $('#customSwitch-{{ $entry->id }}').prop('checked', false);
+                    $('#customSwitch-{{ $entry->id }}-{{ $entry->name }}').prop('checked', false);
                 }
                 else{
-                    $('#customSwitch-{{ $entry->id }}').prop('checked', true);
+                    $('#customSwitch-{{ $entry->id }}-{{ $entry->name }}').prop('checked', true);
                 }
                 new Noty({
                     type: "success",
