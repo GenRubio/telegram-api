eb9d95b037e28f7cfa17e96c7e40e7ada4ccbde3
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-02-25 10:53:16 +0100

    test

--- app/Models/GalleryProduct.php
+++ app/Models/GalleryProduct.php
@@ -108,7 +108,7 @@ class GalleryProduct extends Model
             if ($this->{$attribute_name}) {
                 Storage::disk($disk)->delete('public/' . $this->{$attribute_name});
             }
-            dd($value);
+            dd($value->getClientOriginalExtension());
             $extencion = 'png';
             try{
                 $b64 = $value;
