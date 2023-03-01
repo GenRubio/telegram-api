c35a71a2250fbfed01c394a2cf593c90f5b3f62f
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-03-01 08:06:07 +0100

    test

--- app/Http/Controllers/Api/v1/GetConfigController.php
+++ app/Http/Controllers/Api/v1/GetConfigController.php
@@ -13,7 +13,6 @@ class GetConfigController extends Controller
 {
     public function index(Request $request)
     {
-        Log::info($_SERVER);
         try {
             return response()->json(new ConfigResource(requestAttrEncrypt($request->token)));
         } catch (GenericException | Exception $e) {
