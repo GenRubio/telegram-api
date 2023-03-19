60d60fc2225561db5c8e8c01d986508e78e19390
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-03-19 10:00:32 +0100

    test

--- resources/views/vendor/backpack/crud/buttons/model-flavors.blade.php
+++ resources/views/vendor/backpack/crud/buttons/model-flavors.blade.php
@@ -1,6 +1,4 @@
-@if ($entry->multiple_flavors)
-    <a href="{{ url($crud->route . '/' . $entry->getKey()) }}/product-models-flavor" class="btn btn-sm btn-link"
-        style="color: #7c69ef !important; cursor: pointer !important;">
-        <i class="las la-plus"></i> Sabores
-    </a>
-@endif
+<a href="{{ url($crud->route . '/' . $entry->getKey()) }}/product-models-flavor" class="btn btn-sm btn-link"
+    style="color: #7c69ef !important; cursor: pointer !important;">
+    <i class="las la-plus"></i> Sabores
+</a>
