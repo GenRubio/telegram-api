c69ede6be935609665beb32ede993f85a1ce29a7
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-04-25 09:00:10 +0200

    test

--- resources/views/backpack/pages/env-manager.blade.php
+++ resources/views/backpack/pages/env-manager.blade.php
@@ -1,89 +1,83 @@
 @extends(backpack_view('layouts.top_left'))
 
 @php
-$breadcrumbs = [
-    trans('backpack::crud.admin') => backpack_url('dashboard'),
-    trans('backpack::backup.backups') => false,
-];
+    $breadcrumbs = [
+        trans('backpack::crud.admin') => backpack_url('dashboard'),
+        'Env' => false,
+    ];
 @endphp
 
 @section('header')
-<section class="container-fluid">
-    <h2><span class="text-capitalize">{{ trans('backpack::backup.backups') }}</span></h2>
-</section>
+    <section class="container-fluid">
+        <h2><span class="text-capitalize">Credenciales Env</span></h2>
+    </section>
 @endsection
 
 @section('content')
-{{-- Default box --}}
-<button id="create-new-backup-button" href="" class="btn btn-primary mb-2">
-    <i class="la la-spinner"></i>
-    <i class="la la-plus"></i>
-    <span>{{ trans('backpack::backup.create_a_new_backup') }}</span>
-</button>
+    {{-- Default box --}}
+    <button id="create-new-backup-button" href="" class="btn btn-success mb-2">
+        <i class="las la-cloud-upload-alt"></i>
+        <span>Importar</span>
+    </button>
+    <button id="create-new-backup-button" href="" class="btn btn-primary mb-2">
+        <i class="las la-cloud-download-alt"></i>
+        <span>Descargar plantilla</span>
+    </button>
 
-<div class="card">
-    <div class="card-body p-0">
-        <table class="table table-hover pb-0 mb-0">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>{{ trans('backpack::backup.location') }}</th>
-                    <th>{{ trans('backpack::backup.date') }}</th>
-                    <th class="text-right">{{ trans('backpack::backup.file_size') }}</th>
-                    <th class="text-right">{{ trans('backpack::backup.actions') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                {{-- @foreach ($backups as $key => $backup)
-                <tr>
-                    <td>{{ $key + 1 }}</td>
-                    <td>{{ $backup->diskName }}</td>
-                    <td>{{ $backup->lastModified }}</td>
-                    <td class="text-right">{{ $backup->fileSize }} MB</td>
-                    <td class="text-right">
-                        @if ($backup->downloadLink)
-                        <a class="btn btn-sm btn-link" data-button-type="download" href="{{ $backup->downloadLink }}">
-                            <i class="la la-cloud-download"></i> {{ trans('backpack::backup.download') }}
-                        </a>
-                        @endif
-                        <a class="btn btn-sm btn-link" data-button-type="delete" href="{{ $backup->deleteLink }}">
-                            <i class="la la-trash-o"></i> {{ trans('backpack::backup.delete') }}
-                        </a>
-                    </td>
-                </tr>
-                @endforeach --}}
-            </tbody>
-        </table>
+    <div class="card">
+        <div class="card-body p-0">
+            <table class="table table-hover pb-0 mb-0">
+                <thead>
+                    <tr>
+                        <th>UUID</th>
+                        <th>Tipo</th>
+                        <th>Descripcion</th>
+                        <th class="text-right">{{ trans('backpack::backup.actions') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data as $item)
+                        <tr>
+                            <td>{{ $item['uuid'] }}</td>
+                            <td>{{ $item['type'] }}</td>
+                            <td>{{ $item['description'] }}</td>
+                            <td class="text-right">
+                                <a class="btn btn-sm btn-link" data-button-type="delete" href="">
+                                    <i class="la la-trash-o"></i> {{ trans('backpack::backup.delete') }}
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('after_styles')
-<style>
-    #create-new-backup-button.loading>.la-spinner {
-        display: inherit;
-        animation: rotation 1s steps(8, end) infinite;
-    }
-
-    #create-new-backup-button>.la-spinner,
-    #create-new-backup-button.loading>.la-plus {
-        display: none;
-    }
-
-    @keyframes rotation {
-        from {
-            transform: rotate(0deg);
+    <style>
+        #create-new-backup-button.loading>.la-spinner {
+            display: inherit;
+            animation: rotation 1s steps(8, end) infinite;
         }
 
-        to {
-            transform: rotate(359deg);
+        #create-new-backup-button>.la-spinner,
+        #create-new-backup-button.loading>.la-plus {
+            display: none;
         }
-    }
-</style>
+
+        @keyframes rotation {
+            from {
+                transform: rotate(0deg);
+            }
+
+            to {
+                transform: rotate(359deg);
+            }
+        }
+    </style>
 @endsection
 
 @section('after_scripts')
-<script>
-    
-</script>
+    <script></script>
 @endsection
