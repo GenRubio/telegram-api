8083ce4388adbd60b92b26d5f78ed72c8f6558aa
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-05-29 21:18:02 +0200

    test

--- app/Http/Resources/Api/ProductDetail/ProductProductDetailResource.php
+++ app/Http/Resources/Api/ProductDetail/ProductProductDetailResource.php
@@ -35,7 +35,7 @@ class ProductProductDetailResource extends JsonResource
             'discount' => $this->product->discount,
             'price_with_discount' => $this->product->price_with_discount,
             'brand' => $this->product->productBrand->name,
-            'product_description' => $this->product->description,
+            'product_description' => $this->product->getTranslations('description')[$this->language],
             'multiple_flavors' => $this->product->multiple_flavors,
             'flavors' => count($this->product->productModelsFlavors),
             'shopping' => $this->getTotalProductsBuyed(),
